21fbb1a021e1db066e082e9d9b05c14e02273713
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2014-04-01 20:18:01 +0100

    eval async and scope status

--- js/scope.js
+++ js/scope.js
@@ -1,20 +1,46 @@
 var nop = function(){}
 
 function Scope() {
-  this.watchers = []
+  this.watchers   = [];
+  this.asyncQueue = [];
+  this.phase = null;
 }
 
-Scope.prototype.watch = function(watchFn, listener) {
-  this.watchers.push({watchFn: watchFn, listener: listener || nop })
+Scope.prototype.beginPhase = function(phase) {
+  if(this.phase) throw this.phase + ' already running';
+  this.phase = phase;
 }
 
+Scope.prototype.clearPhase = function() {
+  this.phase = null;
+}
+
+Scope.prototype.watch = function(watchFn, listener, valueComparator) {
+  this.watchers.push({watchFn: watchFn, listener: listener || nop , valueComparator: !! valueComparator })
+}
 
 Scope.prototype.digest = function() {
   var ttl = 10;
-  while(nestedLoop.call(this)) {
-    ttl--;
-    if (ttl <= 0) throw 'Problem to stabilize'
-  }
+  var dirty = false;
+  this.beginPhase('digesting');
+
+  do {
+    
+    while(this.asyncQueue.length) {
+      var task = this.asyncQueue.shift();
+      this.eval(task.expression);
+    }
+
+    dirty = nestedLoop.call(this);
+    if( dirty && !(ttl--)){
+      this.clearPhase();
+      throw 'Problem to stabilize dirty checking !'
+    } 
+
+  } while(dirty)
+
+  this.clearPhase();
+   
 
   function nestedLoop() {
     var that  = this;
@@ -23,9 +49,9 @@ Scope.prototype.digest = function() {
     _.forEach(this.watchers, function(watcher) {
       var newValue = watcher.watchFn(that);
       var oldValue = watcher.value;
-      if(newValue !== oldValue) {
+      if( ! equal(newValue, oldValue, watcher.valueComparator) ) {
         watcher.listener(newValue, oldValue, that);
-        watcher.value = newValue;
+        watcher.value = ( watcher.valueComparator ? _.cloneDeep(newValue) : newValue)
         dirty = true;
       }
     })
@@ -33,21 +59,60 @@ Scope.prototype.digest = function() {
     return dirty;
   }
 
+  function equal(newValue, oldValue, valueComparator) {
+    if(valueComparator) { return _.isEqual(newValue, oldValue) }
+    else {
+      return newValue === oldValue || ( typeof newValue === 'number' && typeof oldValue === 'number' &&
+       isNaN(newValue) && isNaN(oldValue));
+    }
+  }
+
 }
 
+Scope.prototype.eval = function(expression, locals) {
+  return expression(this, locals);
+}
+
+Scope.prototype.apply = function(expression) {
+  try {
+    this.beginPhase('apply');
+    return this.eval(expression);
+  } finally {
+    this.clearPhase();
+    this.digest();
+  }
+}
+
+Scope.prototype.evalAsync = function(expression) {
+  var that = this;
+  
+  if(! this.phase && this.asyncQueue.length > 0) {
+    setTimeout(function(){
+      if(that.asyncQueue.length) {
+        self.digest();
+      }
+    },0)
+  }
+
+  this.asyncQueue.push({ scope: this, expression: expression })
+}
+
+
 var scope = new Scope();
-scope.middleName = 'Hello';
-scope.lastName = 'Thiago';
+scope.asyncCounter = 1;
 
 scope.watch(
+  
   function(scope) {
-    console.log('listener')
-    return scope.middleName;
+    return scope.asyncCounter;
   },
-
+  
   function(newValue, oldValue, scope) {
-    scope.middleName = 'teixeira';
+    scope.evalAsync(function(scope) { scope.asyncCounter++; });
+    console.log("eval - listener: "+ scope.asyncCounter);
   }
+
 );
 
 scope.digest();
+console.log("eval after async: " + scope.asyncCounter);
